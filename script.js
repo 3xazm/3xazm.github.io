5f031daf153bd6c691a358014e75dd6fb5e8898b
Carlos <160207677+CarlosW-sky@users.noreply.github.com>
2025-01-06 15:01:18 -0600

    Update index.htnl

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ const lotteryData = [
     { date: "2025-01-10", mediodia: "--", tarde: "--", noche: "--" },
     { date: "2025-01-09", mediodia: "--", tarde: "--", noche: "--" },
     { date: "2025-01-08", mediodia: "--", tarde: "--", noche: "--" },
-    { date: "2025-01-07", mediodia: "--", tarde: "--", noche: "--" },
-    { date: "2025-01-06", mediodia: "--", tarde: "--", noche: "--" },*/
+    { date: "2025-01-07", mediodia: "--", tarde: "--", noche: "--" },*/
+    { date: "2025-01-06", mediodia: "63", tarde: "--", noche: "--" },
     { date: "2025-01-05", mediodia: "51", tarde: "43", noche: "90" },
     { date: "2025-01-04", mediodia: "97", tarde: "40", noche: "97" },
     { date: "2025-01-03", mediodia: "48", tarde: "32", noche: "63" },
