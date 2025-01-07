bf119a4dd053be7f410e33d27c1822225ffb91b2
Carlos <carloswl010608@gmail.com>
2025-01-07 13:02:58 -0600

    Update index.html

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ const lotteryData = [
     /*
     { date: "2025-01-10", mediodia: "--", tarde: "--", noche: "--" },
     { date: "2025-01-09", mediodia: "--", tarde: "--", noche: "--" },
-    { date: "2025-01-08", mediodia: "--", tarde: "--", noche: "--" },
-    { date: "2025-01-07", mediodia: "--", tarde: "--", noche: "--" },
-    { date: "2025-01-06", mediodia: "--", tarde: "--", noche: "--" },*/
-    { date: "2025-01-05", mediodia: "51", tarde: "43", noche: "--" },
+    { date: "2025-01-08", mediodia: "--", tarde: "--", noche: "--" },*/
+    { date: "2025-01-07", mediodia: "70", tarde: "--", noche: "--" },
+    { date: "2025-01-06", mediodia: "63", tarde: "02", noche: "70" },
+    { date: "2025-01-05", mediodia: "51", tarde: "43", noche: "90" },
     { date: "2025-01-04", mediodia: "97", tarde: "40", noche: "97" },
     { date: "2025-01-03", mediodia: "48", tarde: "32", noche: "63" },
     { date: "2025-01-02", mediodia: "33", tarde: "42", noche: "76" }
